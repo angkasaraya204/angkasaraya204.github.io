5bf1d9d0967b1ba820645f33d77c21454787a5f9
angkasaraya204 <angkasaraya204@gmail.com>
2022-08-18 20:16:47 +0700

    new

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./view/**/*.{html,js}'],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      // animation: {
+      //   "pulse": "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      // },
+      // keyframes: {
+      //   pulse: {
+      //     "0%, 100%": {
+      //       opacity: 100,
+      //       "background": "linear-gradient(to right, #e2e2e2 25%, #d5d5d5 50%, #e2e2e2 100%)"
+      //     },
+      //     "50%": {
+      //       opacity: 0,
+      //       "background": "linear-gradient(to right, #e2e2e2 25%, #d5d5d5 50%, #e2e2e2 100%)"
+      //     },
+      //   },
+      // },
+    },
   },
   plugins: [],
   darkMode: 'class',
--- tailwind/view/js/manualskeletonload.js
+++
@@ -1,9 +0,0 @@
-const intro = document.querySelector('.intro-text');
-const renderCard = () => {
-    intro.textContent = 'Card Title Yo!';
-    intro.classList.remove('loading');
-};
-
-setTimeout(() => {
-    renderCard();
-}, 2500);
