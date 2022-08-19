738aaecaf15c05beaa5a02b7dfd87db35dce2e84
angkasaraya204 <angkasaraya204@gmail.com>
2022-08-19 13:54:23 +0700

    new

--- tailwind/view/js/skeletonload.js
+++ tailwind/view/js/skeletonload.js
@@ -1,39 +1,47 @@
-const $skuloadAbout = document.querySelector(".content-about");
-// Loading finished
-setTimeout(() => {
-    $skuloadAbout.classList.remove("skeleton");
-    $skuloadAbout.querySelectorAll(".hide-about")
-        .forEach(
-            (skuloadAbout) => skuloadAbout.classList.remove("hide-about")
-        );
-}, 3500);
+// const $skuloadAbout = document.querySelector(".content-about");
+// // Loading finished
+// setTimeout(() => {
+//     $skuloadAbout.classList.remove("skeleton");
+//     $skuloadAbout.querySelectorAll(".hide-about")
+//         .forEach(
+//             (skuloadAbout) => skuloadAbout.classList.remove("hide-about")
+//         );
+// }, 3500);
 
-const $skuloadSkills = document.querySelector(".content-skills");
-// Loading finished
-setTimeout(() => {
-    $skuloadSkills.classList.remove("skeleton");
-    $skuloadSkills.querySelectorAll(".hide-skills")
-        .forEach(
-            (skuloadSkills) => skuloadSkills.classList.remove("hide-skills")
-        );
-}, 3500);
+// const $skuloadSkills = document.querySelector(".content-skills");
+// // Loading finished
+// setTimeout(() => {
+//     $skuloadSkills.classList.remove("skeleton");
+//     $skuloadSkills.querySelectorAll(".hide-skills")
+//         .forEach(
+//             (skuloadSkills) => skuloadSkills.classList.remove("hide-skills")
+//         );
+// }, 3500);
 
-const $skuloadPorto = document.querySelector(".content-porto");
-// Loading finished
-setTimeout(() => {
-    $skuloadPorto.classList.remove("skeleton");
-    $skuloadPorto.querySelectorAll(".hide-porto")
-        .forEach(
-            (skuloadPorto) => skuloadPorto.classList.remove("hide-porto")
-        );
-}, 3500);
+// const $skuloadPorto = document.querySelector(".content-porto");
+// // Loading finished
+// setTimeout(() => {
+//     $skuloadPorto.classList.remove("skeleton");
+//     $skuloadPorto.querySelectorAll(".hide-porto")
+//         .forEach(
+//             (skuloadPorto) => skuloadPorto.classList.remove("hide-porto")
+//         );
+// }, 3500);
 
-const $skuloadContact= document.querySelector(".content-contact");
-// Loading finished
-setTimeout(() => {
-    $skuloadContact.classList.remove("skeleton");
-    $skuloadContact.querySelectorAll(".hide-contact")
-        .forEach(
-            (skuloadContact) => skuloadContact.classList.remove("hide-contact")
-        );
-}, 3500);
+// const $skuloadContact= document.querySelector(".content-contact");
+// // Loading finished
+// setTimeout(() => {
+//     $skuloadContact.classList.remove("skeleton");
+//     $skuloadContact.querySelectorAll(".hide-contact")
+//         .forEach(
+//             (skuloadContact) => skuloadContact.classList.remove("hide-contact")
+//         );
+// }, 3500);
+
+// New
+const skeletons = document.querySelectorAll('.skeleton')
+skeletons.forEach((skeletons) => {
+    setTimeout(() => {
+        skeletons.classList.remove('skeleton')
+    }, 3500)
+})
